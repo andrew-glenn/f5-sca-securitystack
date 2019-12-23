97260539ebd2e4fdded6e2ef708d90cef6c2c4f6
Andrew Glenn <andglenn@amazon.com>
2019-12-23 09:31:55 -0600

    Parameter and Resource name adjustments

--- cft-factory/gen_ha_iapp_cft.py
+++ cft-factory/gen_ha_iapp_cft.py
@@ -9,39 +9,39 @@ template = Template()
 
 # add parameters
 pPrefix = template.add_parameter(Parameter(
-    'pPrefix',
+    'Prefix',
     Description='Lambda Function Name Prefix',
     Type='String'
 ))
 
 piAppUrl = template.add_parameter(Parameter(
-    'piAppUrl',
+    'iAppUrl',
     Description='URL to download the BIG-IP AWS HA iApp',
     Default='https://raw.githubusercontent.com/F5Networks/f5-aws-cloudformation/v3.1.0/iApps/f5.aws_advanced_ha.v1.4.0rc3.tmpl',
     Type='String'
 ))
 
 pBigIPMgmt = template.add_parameter(Parameter(
-    'pBigIPMgmt',
+    'BigIPMgmt',
     Description='Active BIG-IP Management IP address',
     Type='String'
 ))
 
 pBigIPRouteTableId = template.add_parameter(Parameter(
-    'pBigIPRouteTableId',
+    'BigIPRouteTableId',
     Description='BIG-IP route table id for HA',
     Type='String'
 ))
 
 pBigIPInterface = template.add_parameter(Parameter(
-    'pBigIPInterface',
+    'BigIPInterface',
     Description='BIG-IP interface for HA',
     Type='String'
 ))
 
 
 pBigIPS3Bucket = template.add_parameter(Parameter(
-    'pBigIPS3Bucket',
+    'BigIPS3Bucket',
     Description='BIG-IP S3 bucket where password is stored',
     Type='String'
 ))
@@ -50,11 +50,11 @@ custom = template.add_resource(CustomResource(
     'CustomLambdaExec',
     # ServiceToken=GetAtt('install-lambda-test-ha-iapp', 'Arn'),
     ServiceToken=Sub('arn:aws:lambda:${AWS::Region}:${AWS::AccountId}:function:${pPrefix}-ha-iapp'),
-    mgmt_ip=Ref('pBigIPMgmt'),
-    iapp_url=Ref('piAppUrl'),
-    route_table_id=Ref('pBigIPRouteTableId'),
-    interface=Ref('pBigIPInterface'),
-    s3_bucket=Ref('pBigIPS3Bucket')
+    mgmt_ip=Ref('BigIPMgmt'),
+    iapp_url=Ref('iAppUrl'),
+    route_table_id=Ref('BigIPRouteTableId'),
+    interface=Ref('BigIPInterface'),
+    s3_bucket=Ref('BigIPS3Bucket')
 ))
 
 # print(template.to_json())
